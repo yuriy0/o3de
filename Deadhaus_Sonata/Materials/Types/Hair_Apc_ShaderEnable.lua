20817a5ab60d3e54e5e52c7daadc7b9c84a200af
antonmic <antonmic@amazon.com>
2021-06-16 21:29:08 -0700

    Removing more irrelevant code from Hair

--- Deadhaus_Sonata/Materials/Types/Hair_Apc_ShaderEnable.lua
+++ Deadhaus_Sonata/Materials/Types/Hair_Apc_ShaderEnable.lua
@@ -13,7 +13,7 @@
 ----------------------------------------------------------------------------------------------------
 
 function GetMaterialPropertyDependencies()
-    return {"opacity.mode", "parallax.textureMap", "parallax.useTexture", "parallax.pdo"}
+    return {"opacity.mode"}
 end
 
 OpacityMode_Opaque = 0
@@ -23,10 +23,6 @@ OpacityMode_TintedTransparent = 3
 
 function Process(context)
     local opacityMode = context:GetMaterialPropertyValue_enum("opacity.mode")
-    local displacementMap = context:GetMaterialPropertyValue_Image("parallax.textureMap")
-    local useDisplacementMap = context:GetMaterialPropertyValue_bool("parallax.useTexture")
-    local parallaxEnabled = displacementMap ~= nil and useDisplacementMap
-    local parallaxPdoEnabled = context:GetMaterialPropertyValue_bool("parallax.pdo")
     
     local depthPass = context:GetShaderByTag("DepthPass")
     local shadowMap = context:GetShaderByTag("Shadowmap")
